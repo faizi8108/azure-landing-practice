b67a9cbf2d9ea3a22791394d0277870180823745
Faizaan <faizand222@gmail.com>
2025-06-25 20:02:32 +0530

    Fix: provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,4 @@
 provider "azurerm" {
   features {}
 
-  client_id       = env.ARM_CLIENT_ID
-  client_secret   = env.ARM_CLIENT_SECRET
-  subscription_id = env.ARM_SUBSCRIPTION_ID
-  tenant_id       = env.ARM_TENANT_ID
 }
