dbfd0eaf56725cf4f8b71246c1c9f9cdc1f338cf
Faizaan <faizand222@gmail.com>
2025-06-25 16:27:19 +0530

    Fix: modified provider.tf and variables.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  features = {}
+  features {}
 
   client_id       = var.client_id
   client_secret   = var.client_secret
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "subscription_id" {}
 variable "tenant_id" {}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
